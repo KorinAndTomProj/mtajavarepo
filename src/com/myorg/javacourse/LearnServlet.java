007ee52e21f0fd37439c43b7bb990d15bdd768fe
korinatar <KorinAtar@gmail.com>
2015-11-11 17:46:08 +0200

    tirgul 3!

---
+++ src/com/myorg/javacourse/LearnServlet.java
@@ -0,0 +1,18 @@
+package com.myorg.javacourse;
+import java.io.IOException;
+import javax.servlet.http.*;
+import com.myorg.javacourse.MathEx3;
+
+@SuppressWarnings("serial")
+public class LearnServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		
+		int x = 1;
+		int y = 1;
+		String result = x + " +  " + y + " = " + (x+y);
+		
+		resp.getWriter().println(result);
+	}
+}
+
---
+++ src/com/myorg/javacourse/MathEx3.java
@@ -0,0 +1,10 @@
+package com.myorg.javacourse;
+
+public class MathEx3 {
+	
+	public static int sum(int x, int y){
+		return x+y;
+		
+	}
+
+}
